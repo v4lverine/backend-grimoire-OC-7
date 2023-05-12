1a079475c185d66734b00486018a7fc7b786155a
Valentine Bontemps <valveryne@hotmail.com>
2023-05-12 09:06:16 +0200

    Ajout routes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
+const Book = require('./models/Book');
+
 mongoose.connect('mongodb+srv://userBackend:pBYllGzvXwkZc4VN@cluster0.m4hrwkz.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -17,18 +19,19 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/auth/signup', (req, res, next) => {
-    
-    next();
-});
+// app.post('/api/auth/signup', (req, res, next) => {
 
-app.post('/api/auth/login', (req, res, next) => {
+// });
 
-    next();
-});
+// app.post('/api/auth/login', (req, res, next) => {
+
+//     next();
+// });
 
 app.get('/api/books', (req, res, next) => {
-
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
   next();
 });
 
@@ -38,22 +41,34 @@ app.get('/api/books/bestrating', (req, res, next) => {
   });
 
 app.get('/api/books/:id', (req, res, next) => {
-
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
     next();
 });
 
 
 app.post('/api/books',(req, res, next) => {
-
+    delete req.body._id;
+    const book = new Book({
+      ...req.body
+    });
+    book.save()
+      .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
     next();
 });  
 
 app.put('/api/books/:id',(req, res, next) => {
-
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.delete('/api/books/:id',(req, res, next) => {
-
+    Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
   next();
 });
 
